59eb5c5e9f36cff6de72270385e9524cd98eed06
LukeDavies12 <luketdavies12@gmail.com>
2023-02-06 15:48:32 -0600

    for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://lukedavies12.github.io',
-  base: '/luke-davies-devfolio',
+  base: '/',
 });
